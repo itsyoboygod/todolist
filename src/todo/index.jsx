6b7512dc3cfe1d28b45ab33f5572a390bc615021
itsyoboygod <m.vini.ac@hotmail.com>
2022-08-25 14:55:34 -0300

    test git deploy

--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -49,6 +49,7 @@ const TodoList = () => {
                 <div className="todo-body">
                     <div className="todo-body-title">
                         <h1>What are the plans for today ?</h1>
+                        <h1>salve dolode !!</h1>
                     </div>
                     <div className="addTask">
                         <div className="addTask-input">
